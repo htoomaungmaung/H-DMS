20ad1522853b335f91159b6cfc022339cfa04aef
Htoo Maung Maung <42893520+htoomaungmaung@users.noreply.github.com>
2018-11-18 22:59:02 +0800

    fetch data from contentful

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import CourseList from "./containers/courseList/courseList";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <CourseList />
+      </div>
+    );
+  }
+}
+
+export default App;
